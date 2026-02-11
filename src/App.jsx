38087bec6f03252ee78a514535a45be7cd59873f
Faisal-Abbas-Saqib <faisalsaqib1@yahoo.com>
2026-02-11 03:12:25 +0100

    new settings

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,28 @@ import Infofood from "./Infofood";
 import Home from "./Home";
 import Header1 from "./assets/Header1";
 import Footer1 from "./assets/Footer1";
+import Header_pic from "./assets/Header_pic.jpg";
 
 function App() {
   return (
     <>
-      <Header1 />
+      {/* 2. Apply the background image to a container or pass it to Header1 */}
+      <div
+        style={{
+          backgroundImage: `url(${Header_pic})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          marginLeft: "8%",
+          color: "rgb(17, 17, 16)",
+          fontSize: "x-large",
+          fontStyle: "italic",
+          width: "200%",
+          textAlign: "left",
+        }}
+      >
+        <Header1 />
+      </div>
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/:mealid" element={<Infofood />} />
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,28 +1,25 @@
 import { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
+import Api_pic from "./assets/Api_pic.jpg"; // Import is correct here
 
 function Home() {
-  const [search, setSearch] = useState(""); // search  for food item
+  const [search, setSearch] = useState("");
   const [data, setData] = useState([]);
-  const [message, setMessage] = useState(""); // show msg if no text entered
-  const [foodFetch, setFoodFetch] = useState(false); // only run useEffect hook if state is true
+  const [message, setMessage] = useState("");
+  const [foodFetch, setFoodFetch] = useState(false);
 
   useEffect(() => {
     if (foodFetch) {
       fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`)
-        .then((res) => {
-          return res.json();
-        })
+        .then((res) => res.json())
         .then((data) => {
-          console.log(data);
           setData(data.meals);
         });
       setFoodFetch(false);
     }
-  }, [foodFetch, search]); // only runs if state is changed
+  }, [foodFetch, search]);
 
   function foodSelect1() {
-    // run function if search button is clicked
     if (search.trim() === "") {
       setMessage("Please enter Food type!");
       setSearch("");
@@ -31,8 +28,11 @@ function Home() {
       setMessage("");
     }
   }
+
   return (
     <div>
+      {/* ADDED: Visual Header with Api_pic */}
+
       <div className="searchBar">
         <input
           type="text"
@@ -40,32 +40,45 @@ function Home() {
           value={search}
           onChange={(e) => setSearch(e.target.value.toLowerCase())}
         />
-        {console.log(search)}
         <button onClick={foodSelect1}>Search</button>
       </div>
-      <h3>{message}</h3> {/* shows msg if no text is entered in search bar*/}
-      <div className="Fetchapi">
-        {" "}
-        {/*shows search food items */}
+
+      <h3 style={{ textAlign: "center", color: "red" }}>{message}</h3>
+
+      <div
+        className="Fetchapi"
+        style={{
+          backgroundImage: `url(${Api_pic})`, // Using the imported variable
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          width: "300%",
+          border: "3px solid rgb(193, 30, 30)",
+          margin: "20px",
+          padding: "0px",
+          width: "505%",
+          minHeight: "350px",
+          marginLeft: "5%",
+          borderStyle: "double",
+        }}
+      >
         {!data ? (
-          <h1>" No data found!" </h1>
+          <h1>"No data found!"</h1>
         ) : (
-          data.map((data2) => {
-            console.log(data2);
-            return (
-              <div className="imagesApi">
-                <img
-                  src={data2.strMealThumb}
-                  width={150}
-                  className="image-border"
-                />
-                <NavLink to={`/${data2.idMeal}`}>
-                  <button>{data2.strMeal}</button>
-                </NavLink>{" "}
-                {/* send mealid to Infofood*/}
-              </div>
-            );
-          })
+          data.map((data2) => (
+            <div className="imagesApi" key={data2.idMeal}>
+              <img
+                src={data2.strMealThumb}
+                width={160}
+                style={{ marginLeft: 120 }}
+                className="image-border"
+                alt={data2.strMeal}
+              />
+
+              <NavLink to={`/${data2.idMeal}`}>
+                <button>{data2.strMeal}</button>
+              </NavLink>
+            </div>
+          ))
         )}
       </div>
     </div>
