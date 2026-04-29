401c28943a38a71f29fa7c93980760a6088b4fca
Faisal-Abbas-Saqib <faisalsaqib1@yahoo.com>
2026-04-29 03:51:30 +0200

    Refactor Infofood component and clean up code

--- src/Infofood.jsx
+++ src/Infofood.jsx
@@ -1,20 +1,22 @@
 import { useState, useEffect } from "react";
-
 import { NavLink, useParams } from "react-router-dom";
 
 const Infofood = () => {
   const { mealid } = useParams();
   const [detail, setDetail] = useState(null);
+
   useEffect(() => {
     fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealid}`)
       .then((response) => response.json())
       .then((data) => {
         setDetail(data.meals[0]);
       });
-  }, [mealid]); // runs only  if meal id is changed
+  }, [mealid]);
+
   if (!detail) {
     return <h2 className="loading">Loading meal details...</h2>;
   }
+
   return (
     <>
       <div className="foodInfo">
@@ -33,20 +35,11 @@ const Infofood = () => {
             ))}
         </div>
         <NavLink to="/">
-          <button
-            className="homeButton"
-            style={{
-              backgroundColor: "#007bff",
-              color: "#fff",
-              padding: "10px",
-              borderRadius: "4px",
-            }}
-          >
-            Home
-          </button>
+          <button className="homeButton">← Back to Home</button>
         </NavLink>
       </div>
     </>
   );
 };
+
 export default Infofood;
